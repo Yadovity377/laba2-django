5aa3be7a13e04ca4e8be910d33682bcacfb53f19
Yadovity377 <seredov3000@gmail.com>
2025-11-14 11:06:29 +0500

    Обновлены маршруты

--- articles/urls.py
+++ articles/urls.py
@@ -6,13 +6,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
-    
-    # Книги
+    path('books/', views.books_list, name='books_list'),
     path('books/add/', views.add_book, name='add_book'),
     path('books/upload/', views.upload_book, name='upload_book'),
-    path('books/', views.books_list, name='books_list'),
     path('books/delete/', views.delete_book, name='delete_book'),
-    path('books/delete/<int:pk>/', views.delete_book_db, name='delete_book_db'),
+    path('books/delete-db/<int:pk>/', views.delete_book_db, name='delete_book_db'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/search/', views.book_search, name='book_search'),
 ]
